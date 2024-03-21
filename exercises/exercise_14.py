694c0f42f5af2501e51c07316d170aa66e410c90
MartinDao23 <161600728+MartinDao23@users.noreply.github.com>
2024-03-21 10:25:44 +0000

    14-18

---
+++ exercises/exercise_14.py
@@ -0,0 +1,6 @@
+number = int(input("How many integers will be entered: "))
+sum = 0
+for i in range(1, number+1):
+    entered = int(input("Enter a number: "))
+    sum += 1 if entered == 0 else + 0
+print(sum)
