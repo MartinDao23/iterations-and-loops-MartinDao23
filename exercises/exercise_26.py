925ec08a12a74041cb8414cb67dd10f1fff630a8
MartinDao23 <161600728+MartinDao23@users.noreply.github.com>
2024-03-27 09:45:28 +0000

    26-31

---
+++ exercises/exercise_26.py
@@ -0,0 +1,9 @@
+n = int(input("Enter a number"))
+count = 0
+for i in range (100,1000):
+    ones = i%10
+    tens = (i//10)%10
+    hundreds = i//100
+    if (ones + tens + hundreds) == n:
+        count += 1
+print(count)
